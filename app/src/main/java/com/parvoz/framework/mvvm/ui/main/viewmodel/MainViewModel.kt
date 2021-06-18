5b7cc33b841f25e62b2834f43dcdae42c1bdac58
Azizbek Ismoilov <azizismoilov495@gmail.com>
2021-06-18 11:56:31 +0500

    i updated some library

--- app/src/main/java/com/parvoz/framework/mvvm/ui/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/parvoz/framework/mvvm/ui/main/viewmodel/MainViewModel.kt
@@ -1,14 +1,16 @@
 package com.parvoz.framework.mvvm.ui.main.viewmodel
 
-import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
 import com.parvoz.framework.mvvm.data.model.User
 import com.parvoz.framework.mvvm.data.repository.MainRepository
 import com.parvoz.framework.mvvm.utils.NetworkHelper
 import com.parvoz.framework.mvvm.utils.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainViewModel @ViewModelInject constructor(
+@HiltViewModel
+class MainViewModel @Inject constructor(
     private val mainRepository: MainRepository,
     private val networkHelper: NetworkHelper
 ) : ViewModel() {
